2c8604ec47d883d4b546b76f4b934d8a4d90036d
jaromeroibz <137736111+jaromeroibz@users.noreply.github.com>
2024-02-01 18:59:26 +0000

    se avanzó en rutas

--- src/api/models.py
+++ src/api/models.py
@@ -1,11 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
+from enum import Enum
 
 db = SQLAlchemy()
-
+    
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean(), unique=False, nullable=False)   
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -14,6 +22,429 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Address(db.Model):
+    __tablename__='address'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    directions = db.Column(db.String(120), unique=False, nullable=False)
+    district = db.Column(db.String(120), unique=False, nullable=False)
+    province = db.Column(db.String(120), unique=False, nullable=False)
+    state = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Address {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "directions": self.directions,
+            "district": self.district,
+            "province": self.province,
+            "state": self.state     
+        }
+    
+class UserAddress(db.Model):
+    __tablename__ = 'user_address'
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    address_id = db.Column(db.Integer, db.ForeignKey('address.id'))
+    is_default = db.Column(db.Boolean(), unique=False, nullable=False)
+    user = db.relationship(User)
+    address = db.relationship(Address)
+
+    def __repr__(self):
+        return f'<UserAddress {self.id}>' 
+    
+    def serialize(self):
+        return{
+            "user_id": self.user_id,
+            "address_id": self.address_id,
+            "is_default": self.is_default
+        }
+    
+class ProductCategory(db.Model):
+    __tablename__ = 'product_category'
+    id = db.Column(db.Integer, primary_key = True)
+    category_name = db.Column(db.String(80))    
+
+    def __repr__(self):
+        return f'<ProductCategory {self.id}>' 
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "category_name": self.category_name
+        }
+
+class Promotion(db.Model):
+    __tablename__ = 'promotion'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    discount_rate = db.Column(db.Integer, unique=False, nullable=True)
+    start_date = db.Column(db.String(20), unique=False, nullable=False)
+    end_date = db.Column(db.String(20), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Promotion {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "discount_rate": self.discount_rate,
+            "start_date": self.start_date,
+            "end_date": self.end_date
+        }
+
+class PromotionCategory(db.Model):
+    __tablename__='promotion_category'
+    id = db.Column(db.Integer, primary_key = True)
+    category_id = db.Column(db.Integer, db.ForeignKey('product_category.id'))
+    promotion_id = db.Column(db.Integer, db.ForeignKey('promotion.id'))
+    category = db.relationship(ProductCategory)
+    promotion = db.relationship(Promotion)
+
+    def __repr__(self):
+        return f'<PromotionCategory {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "category_id": self.category_id,
+            "promotion_id": self.promotion_id
+        }
+
+class Variation(db.Model):
+    __tablename__ = 'variation'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), nullable = False, unique = True)
+    category_id = db.Column(db.Integer, db.ForeignKey('product_category.id'))
+    category = db.relationship(ProductCategory)
+
+    def __repr__(self):
+        return f'<Variation {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name
+        }
+
+class VariationOption(db.Model):
+    __tablename__ = 'variation_option'
+    id = db.Column(db.Integer, primary_key = True)
+    value = db.Column(db.String(80), nullable = False, unique = True)
+    variation_id = db.Column(db.Integer, db.ForeignKey('variation.id'))
+    variation = db.relationship(Variation)
+
+    def __repr__(self):
+        return f'<VariationOption {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "value": self.value
+        }
+
+class Products(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('product_category.id'))
+    category = db.relationship(ProductCategory)
+    #product_image figure out how to add image    
+
+
+    def __repr__(self):
+        return f'<Products {self.id}>' 
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+        }    
+    
+class ProductItem(db.Model):
+    __tablename__ = 'product_item'
+    id = db.Column(db.Integer, primary_key = True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    products = db.relationship(Products)
+    sku = db.Column(db.Integer, nullable = False, unique = True)
+    qty_in_stock = db.Column(db.Integer, nullable = False, unique = False)
+    price = db.Column(db.Integer, nullable = False, unique = False)
+    #product_image figure out how to add image    
+
+    def __repr__(self):
+        return f'<ProductItem {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "product_id": self.product_id,
+            "sku": self.sku,
+            "qty_in_stock": self.qty_in_stock,
+            "price": self.price,
+            "product_image": self.product_image
+        }
+
+class ProductConfiguration(db.Model):
+    __tablename__ = 'product_configuration'
+    id = db.Column(db.Integer, primary_key = True)
+    product_item_id = db.Column(db.Integer, db.ForeignKey('product_item.id'))
+    variation_option_id = db.Column(db.Integer, db.ForeignKey('variation_option.id'))
+    product_item = db.relationship(ProductItem)
+    variation_option = db.relationship(VariationOption)
+
+    def __repr__(self):
+        return f'<ProductConfiguration {self.id}>'
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "product_item_id": self.product_item_id,
+            "variation_option_id": self.variation_option_id
+        }    
+    
+class PaymentType(db.Model):
+    __tablename__ = 'payment_type'
+    id = db.Column(db.Integer, primary_key = True)
+    payment_type = db.Column(db.String(80), nullable = False, unique = True)
+
+    def __repr__(self):
+        return f'<PaymentType {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "payment_type": self.payment_type
+        }
+
+class UserPaymentMethod(db.Model):
+    __tablename__ = 'user_payment_method'
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    payment_type_id = db.Column(db.Integer, db.ForeignKey('payment_type.id'))
+    provider = db.Column(db.String(80), nullable = False, unique = False)
+    account_number = db.Column(db.String(80), nullable = False, unique = False)
+    expiry_date = db.Column(db.String(5), nullable = False, unique = False)
+    is_default = db.Column(db.Boolean(), unique=False, nullable=False)
+    payment_type = db.relationship(PaymentType)
+    user = db.relationship(User)
+
+    def __repr__(self):
+        return f'<UserPaymentMethod {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "user_id": self.user_id,
+            "payment_type_id": self.payment_type_id,
+            "provider": self.provider,
+            "account_number": self.account_number,
+            "expiry_date":self.expiry_date,
+            "is_default": self.is_default
+        }
+    
+class ShoppingCart(db.Model):
+    __tablename__ = 'shopping_cart'
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+
+    def __repr__(self):
+        return f'<ShoppingCart {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "user_id": self.id
+        }
+
+class ShoppingCartItem(db.Model):
+    __tablename__ = 'shopping_cart_item'
+    id = db.Column(db.Integer, primary_key = True)
+    cart_id = db.Column(db.Integer, db.ForeignKey('shopping_cart.id'))
+    product_item_id = db.Column(db.Integer, db.ForeignKey('product_item.id'))
+    qty = db.Column(db.Integer, nullable = False, unique = False)
+
+    def __repr__(self):
+        return f'<ShoppingCartItem>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "cart_id": self.cart_id,
+            "product_id": self.product_id
+        }
+
+class OrderStatus(Enum): 
+
+    ordered = 'ordered',
+    processed = 'processed',
+    delivered = 'delivered'
+
+class ShippingMethod(db.Model):
+    __tablename__ = 'shipping_method'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), nullable = False, unique = True)
+    price = db.Column(db.Integer, nullable = False, unique = False)
+
+    def __repr__(self):
+        return f'<ShippingMethod>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "price": self.price
+        }
+    
+class ShopOrder(db.Model):
+    __tablename__ = 'shop_order'
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_payment_method_id = db.Column(db.Integer, db.ForeignKey('user_payment_method.id'))
+    order_date = db.Column(db.String(80), nullable = False, unique = False)
+    shipping_address_id = db.Column(db.Integer, db.ForeignKey('address.id'))
+    order_total = db.Column(db.Integer, nullable = False, unique = False)
+    order_status = db.Column(db.String(80), nullable = False, unique = False)
+    shipping_method_id = db.Column(db.Integer, db.ForeignKey('shipping_method.id'))
+    shipping_method = db.relationship(ShippingMethod)
+    user_payment_method = db.relationship(UserPaymentMethod)
+    user = db.relationship(User)
+    shipping_address = db.relationship(Address)
+    
+
+    def __repr__(self):
+        return f'<ShopOrder {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "user_id": self.user_id,
+            "user_payment_method_id": self.user_payment_method_id,
+            "order_date": self.order_date,
+            "shipping_address_id": self.shipping_address_id,
+            "order_total": self.order_total,
+            "order_status": self.order_status,
+            "shipping_method_id": self.shipping_method_id
+        }
+    
+class OrderLine(db.Model):
+    __tablename__ = 'order_line'
+    id = db.Column(db.Integer, primary_key = True)
+    product_item_id = db.Column(db.Integer, db.ForeignKey('product_item.id'))
+    order_id = db.Column(db.Integer, db.ForeignKey('shop_order.id'))
+    qty = db.Column(db.Integer, nullable = False, unique = False)
+    order = db.relationship(ShopOrder)
+    product_item = db.relationship(ProductItem)
+
+    def __repr__(self):
+        return f'<OrderLine {self.id}>'
+    
+    def serialize(self):
+        return {
+
+            "id": self.id,
+            "product_id": self.product_id,
+            "order_id": self.order_id,
+            "qty": self.qty
+
+        }
+
+class UserReview(db.Model):
+    __tablename__ = 'user_review'
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    ordered_product_id = db.Column(db.ForeignKey('order_line.id'))
+    user = db.relationship(User)
+    ordered_product = db.relationship(OrderLine)
+    rating_value = db.Column(db.Integer, nullable = False, unique=False)
+    comment = db.Column(db.Text, nullable = True, unique = False )
+
+    def __repr__(self):
+        return f'<UserReview> {self.id}'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.id,
+            "ordered_product_id": self.id,
+            "rating_value": self.rating_value,
+            "comment": self.comment
+        }
+
+
+class UserImg(db.Model):
+    __tablename__ = 'img'
+    id = db.Column(db.Integer, primary_key = True)
+    img = db.Column(db.Text, unique=True, nullable=False)
+    name = db.Column(db.Text, nullable = False)
+    mimetype = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = relationship(User)
+  
+    def __repr__(self):
+        return f'<Img {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "img": self.img,
+            "name": self.name,
+            "mimetype": self.mimetype,
+            "user_id": self.user_id
+        }
+    
+class ProductImg(db.Model):
+    __tablename__ = 'product_img'
+    id = db.Column(db.Integer, primary_key = True)
+    img = db.Column(db.Text, unique=True, nullable=False)
+    name = db.Column(db.Text, nullable = False)
+    mimetype = db.Column(db.Text, nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    product = relationship(Products)
+  
+    def __repr__(self):
+        return f'<Img {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "img": self.img,
+            "name": self.name,
+            "mimetype": self.mimetype,
+            "product_id": self.product_id
+        }
+
+class PaymentTypeImg(db.Model):
+    __tablename__ = 'payment_type_img'
+    id = db.Column(db.Integer, primary_key = True)
+    img = db.Column(db.Text, unique=True, nullable=False)
+    name = db.Column(db.Text, nullable = False)
+    mimetype = db.Column(db.Text, nullable=False)
+    payment_type_id = db.Column(db.Integer, db.ForeignKey('payment_type.id'))
+    payment_type = relationship(PaymentType)
+  
+    def __repr__(self):
+        return f'<Img {self.id}>'
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "img": self.img,
+            "name": self.name,
+            "mimetype": self.mimetype,
+            "payment_type_id": self.payment_type_id
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,22 +1,299 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from flask import Flask, request, jsonify, url_for, Blueprint, Response
+from api.models import db, User, Address, UserAddress, ProductCategory, Promotion, PromotionCategory, Products, ProductItem
+from api.models import Variation, VariationOption, ProductConfiguration, PaymentType, UserPaymentMethod, ShoppingCart, ShoppingCartItem
+from api.models import OrderStatus, ShippingMethod, ShopOrder, OrderLine, UserReview
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+# from werkzeug.utils import secure_filename
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+# @api.route('/upload', methods=['POST'])
+# def upload():
+#     pic = request.files['pic']
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+#     if not pic:
+#        return 'No image uploaded', 400
+    
+#     filename = secure_filename(pic.filename)
+#     mimetype = pic.mimetype
+
+#     img = Img(img=pic.read(), mimetype=mimetype)
+#     db.session.add(img)
+#     db.session.commit()
+
+#     return 'img has been uploaded', 200
+
+# @api.route('/get_img/<int:id>', methods=["GET"])
+# def get_img(id):
+#     img = Img.query.filter_by(id=id).first()
+#     if not img:
+#         return 'No img with that id', 404
+    
+#     return Response(img.img, mimetype=img.mimetype)
+
+@api.route("/admin_login", methods=["POST"])
+def admin_login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email).first()
+   
+    if user is None:
+        return jsonify({"msg": "User is not registered"}), 401
+    
+    if user.is_admin is False:
+        return jsonify({"msg": "Access forbidden"}), 403
+
+    if password != user.password :
+        return jsonify({"msg": "Wrong password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email).first()
+   
+    if user is None:
+        return jsonify({"msg": "User is not registered"}), 401
+
+    if password != user.password :
+        return jsonify({"msg": "Wrong password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+@api.route("/signup", methods=["POST"])
+def signup():
+    body = request.get_json()
+    user = User.query.filter_by(email=body["email"]).first()
+
+    if user == None:
+        user = User(name=body["name"], email=body["email"], password=body["password"], is_active=True, is_admin=False)
+
+        db.session.add(user)
+        db.session.commit()
+        user_info = user.serialize()
+        access_token = create_access_token(identity=user_info["id"])
+        user_info['access_token']=access_token
+
+        return jsonify(user_info), 200
+    else:
+        return jsonify({"msg": "user already exists with this email address"}), 401
+
+@api.route("/admin_signup", methods=["POST"])
+def admin_signup():
+    body = request.get_json()
+    user = User.query.filter_by(email=body["email"]).first()
+
+    if user == None:
+        user = User(name=body["name"], email=body["email"], password=body["password"], is_active=True, is_admin=True)
+
+        db.session.add(user)
+        db.session.commit()
+        user_info = user.serialize()
+        access_token = create_access_token(identity=user_info["id"])
+        user_info['access_token']=access_token
+
+        return jsonify(user_info), 200
+    else:
+        return jsonify({"msg": "user already exists with this email address"}), 401
+    
+@api.route('/get_addresses', methods=['GET'])
+@jwt_required()
+def get_addresses():
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(email=current_user).first()
+    user_info = user.serialize()
+    all_addresses = UserAddress.query.filter_by(user_id=user_info["id"]).all()
+    result = list(map(lambda item: item.serialize(), all_addresses))
+
+    return jsonify(result) 
+
+@api.route('/add_address', methods=['POST'])
+@jwt_required()
+def add_address():
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(email=current_user).first()
+    user_info = user.serialize()
+    current_user_id = user_info["id"]
+
+    body = request.get_json()
+
+    address = Address(
+        name = body['name'],
+        directions = body['directions'],
+        district = body['district'],
+        province = body['province'],
+        state = body['state']
+    )
+    db.session.add(address)
+    db.session.commit()
+
+    address_created = Address.query.filter_by(directions=body['directions']).first()
+    address_created_info = address_created.serialize()
+    user_address = UserAddress(
+        user_id = current_user_id,
+        address_id = address_created_info["id"],
+        is_default = body['is_default']
+    )
+    db.session.add(user_address)
+    db.session.commit()
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "message": "Address created"
     }
 
     return jsonify(response_body), 200
+
+@api.route('/update_address/<int:address_id>', methods =['PUT'])
+@jwt_required()
+def update_address(address_id):
+    body = request.get_json()
+    update_address = Address.query.filter_by(id=address_id).first()
+    print(update_address)
+    print(body)
+    if body['name']: update_address.name = body['name']
+    if body['directions']: update_address.directions = body['directions']
+    if body['district']: update_address.district = body['district']
+    if body['province']: update_address.province = body['province']
+    if body['state']: update_address.state = body['state']
+
+    db.session.commit()
+
+    response_body = {
+        "message": "Address updated"
+    }
+      
+    return jsonify(response_body), 200
+
+
+@api.route('/delete_address/<int:address_id>', methods =['DELETE'])
+def delete_address(address_id):
+    delete_address = Address.query.filter_by(id=address_id).first()
+
+    db.session.delete(delete_address)
+    db.session.commit()
+
+    response_body = {
+        "message": "Address deleted"
+    }
+      
+    return jsonify(response_body), 200
+
+@api.route('/get_products', methods=['GET'])
+def get_products():
+    
+    all_products = ProductItem.query.all()
+    result = list(map(lambda item: item.serialize(), all_products))
+
+    return jsonify(result) 
+
+@api.route('/add_products', methods=['POST'])
+@jwt_required()
+def add_products():
+    
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(email=current_user).first()
+    body = request.get_json()
+
+    if user.is_admin is True:
+
+        product = Products(
+        name = body['name'],
+        description = body['description'],
+        )
+        db.session.add(product)
+        db.session.commit()
+
+    response_body = {
+        "message": "Product created"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/add_variation', methods=['POST'])
+@jwt_required()
+def add_variation():
+    
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(email=current_user).first()
+    body = request.get_json()
+
+    if user.is_admin is True:
+
+        variation = Variation(
+        name = body['name']
+        )
+        db.session.add(variation)
+        db.session.commit()
+
+    response_body = {
+        "message": "Variation created"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/add_variation_option/<int:variation_id>', methods=['POST'])
+@jwt_required()
+def add_variation_option(variation_id):
+    
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(email=current_user).first()
+    body = request.get_json()
+
+    if user.is_admin is True:
+
+        variation_option = VariationOption(
+        value = body['value'],
+        variation_id = variation_id
+        )
+        db.session.add(variation_option)
+        db.session.commit()
+
+    response_body = {
+        "message": "Variation created"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/add_product_item/<int:product_id>', methods=['POST'])
+@jwt_required()
+def add_product_item(product_id):
+    
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(email=current_user).first()
+    body = request.get_json()
+
+    if user.is_admin is True:
+
+        product_item = ProductItem(
+        product_id = product_id,
+        sku = body['sku'],
+        qty_in_stock = body['qty_in_stock'],
+        #product_image check how to add image
+        price = body['price']
+        )
+        db.session.add(product_item)
+        db.session.commit()
+
+    response_body = {
+        "message": "Product item created"
+    }
+
+    return jsonify(response_body), 200
+
+
